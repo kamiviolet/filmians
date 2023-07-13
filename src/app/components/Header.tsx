7caef56f7d5f1b55c9cd94d0f3ca958661dcb986
kamiviolet <kamiviolet@gmail.com>
2023-07-13 22:10:21 +0100

    uninstall tailwind and install styled components

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Header() {
+  return (
+    <>
+      <Head>
+        <title>Community Feed</title>
+        <meta
+          name='description'
+          content='This is a Community Feed project build with React'
+        />
+      </Head>
+    </>
+  );
+}
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import StyledComponentsRegistry from '../lib/registry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
